bd4d82ddb7bd6ccb6f6c0f21c746794b92993444
Kris Noble <kris@simianstudios.com>
2015-04-20 10:42:05 +0100

    fix

--- Source/content_script.js
+++ Source/content_script.js
@@ -29,39 +29,40 @@ function walk(node)
 
 function handleText(textNode) 
 {
-	if(textNode.parentElement.nodeName == 'input' || textNode.parentElement.nodeName == 'textarea')
-	var v = textNode.nodeValue;
-	var rand = Math.random();
-	
-	if(rand > 0.49) {
-		v = v.replace(/\bMigrants\b/g, "People");
-		v = v.replace(/\bmigrants\b/g, "people fleeing a terrible situation");
-		v = v.replace(/\bMigrant\b/g, "Person");
-		v = v.replace(/\bmigrant\b/g, "person fleeing persecution or war");
-	} else {
-		v = v.replace(/\bMigrants\b/g, "People seeking a better life");
-		v = v.replace(/\bmigrants\b/g, "people");
-		v = v.replace(/\bMigrant\b/g, "Person seeking a safe place to live");
-		v = v.replace(/\bmigrant\b/g, "person");
+	if(textNode.parentElement.nodeName != 'input' && textNode.parentElement.nodeName != 'textarea'){
+		var v = textNode.nodeValue;
+		var rand = Math.random();
+		
+		if(rand > 0.49) {
+			v = v.replace(/\bMigrants\b/g, "People");
+			v = v.replace(/\bmigrants\b/g, "people fleeing a terrible situation");
+			v = v.replace(/\bMigrant\b/g, "Person");
+			v = v.replace(/\bmigrant\b/g, "person fleeing persecution or war");
+		} else {
+			v = v.replace(/\bMigrants\b/g, "People seeking a better life");
+			v = v.replace(/\bmigrants\b/g, "people");
+			v = v.replace(/\bMigrant\b/g, "Person seeking a safe place to live");
+			v = v.replace(/\bmigrant\b/g, "person");
+		}
+		
+		rand = Math.random();
+		
+		if(rand > 0.49) {
+			v = v.replace(/\bImmigrants\b/g, "People");
+			v = v.replace(/\bimmigrants\b/g, "people seeking a better life");
+			v = v.replace(/\bImmigrant\b/g, "Person");
+			v = v.replace(/\bimmigrant\b/g, "person fleeing violence or economic hardship");
+		} else {
+			v = v.replace(/\bImmigrants\b/g, "People seeking a better life");
+			v = v.replace(/\bimmigrants\b/g, "people");
+			v = v.replace(/\bImmigrant\b/g, "Person seeking a safe place to live");
+			v = v.replace(/\bimmigrant\b/g, "person");
+		}
+		
+		v = v.replace(/\bKatie Hopkins\b/g, "A Professional Troll");
+		
+		textNode.nodeValue = v;
 	}
-	
-	rand = Math.random();
-	
-	if(rand > 0.49) {
-		v = v.replace(/\bImmigrants\b/g, "People");
-		v = v.replace(/\bimmigrants\b/g, "people seeking a better life");
-		v = v.replace(/\bImmigrant\b/g, "Person");
-		v = v.replace(/\bimmigrant\b/g, "person fleeing violence or economic hardship");
-	} else {
-		v = v.replace(/\bImmigrants\b/g, "People seeking a better life");
-		v = v.replace(/\bimmigrants\b/g, "people");
-		v = v.replace(/\bImmigrant\b/g, "Person seeking a safe place to live");
-		v = v.replace(/\bimmigrant\b/g, "person");
-	}
-	
-	v = v.replace(/\bKatie Hopkins\b/g, "A Professional Troll");
-	
-	textNode.nodeValue = v;
 }
 
 function nodeInsertedCallback(event) { // http://stackoverflow.com/a/8887226/1646470
